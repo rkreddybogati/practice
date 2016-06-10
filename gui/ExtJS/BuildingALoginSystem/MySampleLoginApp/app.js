50e1c7fd9785abfedf62082ac04ac39c7dc4878e
Ramakrishna Reddy <rkreddy.bogati@gmail.com>
2016-06-10 12:28:03 +0530

    Changes for Building Login System.

--- gui/ExtJS/BuildingALoginSystem/MySampleLoginApp/app.js
+++ gui/ExtJS/BuildingALoginSystem/MySampleLoginApp/app.js
@@ -16,7 +16,8 @@ Ext.application({
     // will gain a "viewport" plugin if it does not extend Ext.Viewport. With the
     // modern toolkit, the main view will be added to the Viewport.
     //
-    mainView: 'MySampleLoginApp.view.main.Main'
+    // mainView: 'MySampleLoginApp.view.main.Main'
+    mainView: 'MySampleLoginApp.view.login.Login'
 	
     //-------------------------------------------------------------------------
     // Most customizations should be made to MySampleLoginApp.Application. If you need to
--- gui/ExtJS/BuildingALoginSystem/MySampleLoginApp/app/Application.js
+++ gui/ExtJS/BuildingALoginSystem/MySampleLoginApp/app/Application.js
@@ -12,8 +12,24 @@ Ext.define('MySampleLoginApp.Application', {
         // TODO: add global / shared stores here
     ],
     
+    views: [
+        'MySampleLoginApp.view.login.Login',
+        'MySampleLoginApp.view.main.Main'
+    ],
+	
     launch: function () {
-        // TODO - Launch the application
+	// It's important to note that this type of application could use
+        // any type of storage, i.e., Cookies, LocalStorage, etc.
+        var loggedIn;
+
+        // Check to see the current value of the localStorage key
+        loggedIn = localStorage.getItem("TutorialLoggedIn");
+
+        // This ternary operator determines the value of the TutorialLoggedIn key.
+        // If TutorialLoggedIn isn't true, we display the login window,
+        // otherwise, we display the main view
+        Ext.widget(loggedIn ? 'app-main' : 'login')
+       
     },
 
     onAppUpdate: function () {
